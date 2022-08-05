c1f224a7ba960f11608ab50f70a50ab10a905e8d
ruthwekesa <wekesaruth569@gmail.com>
2022-08-05 11:59:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 40;
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
+const { name, height, message } = require("../index.js");
+
+describe("what-is-a-test", () => {
+  describe("Name", () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual("Susan");
+    });
+  });
+
+  describe("Height", () => {
+    it("is less than 40", () => {
+      expect(height).toBeLessThan(40);
+    });
+  });
+
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
+});
